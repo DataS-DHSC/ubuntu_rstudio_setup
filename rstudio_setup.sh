250d37b4fbc9519ef4caf03df355df9a51d156f0
Williamson <Barney.Williamson@dhsc.gov.uk>
2022-04-08 14:30:22 +0100

    first commit

---
+++ rstudio_setup.sh
@@ -0,0 +1,14 @@
+# Install R
+sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/'
+sudo apt install r-base
+
+# Install rstudio server
+sudo apt-get install gdebi-core
+wget https://download2.rstudio.org/server/xenial/amd64/rstudio-server-1.4.1103-amd64.deb
+sudo gdebi rstudio-server-1.4.1103-amd64.deb
+
+
+# Install tidyverse dependencies
+sudo apt install libssl-dev libcurl4-openssl-dev
